c46b0fec5fafcc8fadd95c7359738c98ffc86b56
Tim Cross <theophilusx@gmail.com>
2021-02-13 11:34:26 +1100

    Add execTransactionSQL method

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@
 const { Client, Pool } = require("pg");
 const copyFrom = require("pg-copy-streams").from;
 
+const mkResult = (rslt, accRslt) => {
+  if (!accRslt) {
+    return rslt;
+  }
+  accRslt.rowCount += rslt.rowCount;
+  accRslt.rows = [...accRslt.rows, ...rslt.rows];
+  return accRslt;
+};
+
 class PgHelper {
   constructor(config) {
     this.poolError = null;
@@ -118,6 +127,78 @@ class PgHelper {
       if (this.doRelease) {
         await this.releaseConnection(con);
         this.doRelease = false;
+      }
+    }
+  }
+
+  /**
+   * @async
+   *
+   * Execute statement within a transaction block. If all statement executions succeed without
+   * failure, the transaction is committed. If an error occurs, the transaction is rolled back.
+   * The stmt argument is a string SQL statement, usually with placeholder parameters. The
+   * params argument is an array of arrays. Each sub-array contains the values to be applied
+   * to the placeholder arguments in the statement.
+   *
+   * The return value is an accumulated result set object. The rowcCount property is an accumulation
+   * of the rowCount property for each result set returned from each execution of the statement. The
+   * rows property is an accumulation of all the rows values from each result set returned from all
+   * statement executions.
+   *
+   * The con parameter is an optional connection object. If provided, it will be used for the execution
+   * of statements. If not supplied, the method will retrieve a new connection from the pool and return
+   * it on completion.
+   *
+   * @param {String} stmt - an SQL statement to execute. Placeholder arguments supported
+   * @param {Array} params - (Optional) an array of arrays where each sub-array are the values to use for
+   *                         placeholder arguments. One sub-array used for each statement execution.
+   * @param {Object} con - (Optionsl) a connection object to use for execution of statements.
+   *
+   * @throws {Error} if an error is raised
+   */
+  async execTransactionSQL(stmt, params = [], con) {
+    let result;
+
+    try {
+      if (!con) {
+        con = await this.getConnection();
+        this.doRelease = true;
+      }
+      await con.query("BEGIN");
+      if (params.length) {
+        for (let p of params) {
+          let rslt = await con.query(stmt, p);
+          result = mkResult(rslt, result);
+        }
+      } else {
+        result = await con.query(stmt);
+      }
+      await con.query("COMMIT");
+      if (this.poolError) {
+        let e = new Error(this.poolError.message);
+        this.poolError = null;
+        throw e;
+      }
+      return result;
+    } catch (err) {
+      try {
+        await con.query("ROLLBACK");
+        throw err;
+      } catch (err2) {
+        throw new Error(
+          `execTransactionSQL: ${err2.message} Statement: ${stmt}`
+        );
+      }
+    } finally {
+      try {
+        if (this.doRelease) {
+          await this.releaseConnection(con);
+          this.doRelease = false;
+        }
+      } catch (err) {
+        throw new Error(
+          `execTransactionSQL: Error releasing connection: ${err.message}`
+        );
       }
     }
   }
